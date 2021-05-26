422c7e3390589768ddcf4efba94f03c5c491a5e7
Igor Monteiro Vieira <igor@br.ibm.com>
2021-05-25 22:55:42 -0300

    darkmode

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Developed by https://github.com/285187
+# Developed by https://github.com/igormv28
 from flask import Flask, g, redirect, url_for, request
 from flask_oidc import OpenIDConnect
 import uuid
